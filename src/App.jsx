f2a1290f68b93920edb013cf7814d5f82fa36ebd
jamesnewman12 <mantaa@gmail.com>
2020-09-14 04:57:01 -0400

    added buttons to redirect at top of landing page, still needs refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
 import Main from './components/Main.jsx';
+import About from "./components/About.jsx";
+import { render } from "react-dom";
 
-
-
-export default class holla extends React.Component {
-  render() {
-    return (
-      
-        <React.Fragment>
-          <Main />
-        </React.Fragment>
-       
-      
-    );
+export default class App extends Main {
+  weDoThis() {
+  
+  return (
+    <div>
+      <Main>
+    </Main>
+    </div>
+  )
   }
-}
+  constructor() {
+      super();
+      this.state = {
+        name: "React",
+        showHideDemo1: false,
+        showHideDemo2: false
+      };
+      this.hideComponent = this.hideComponent.bind(this);
+  }
+  
+  hideComponent(name) {
+    console.log(name);
+    switch (name) {
+      case "showHideDemo1":
+        this.setState({ showHideDemo1: !this.state.showHideDemo1 });
+        break;
+      case "showHideDemo2":
+        this.setState({ showHideDemo2: !this.state.showHideDemo2 });
+        break;
+      default:
+        
+    }
+  }
+  
 
+        
+      render() {
+        const { showHideDemo1, showHideDemo2 } = this.state;
+        return (
+      <div>
+          {showHideDemo1 && <Main />}
+          <hr />
+          {showHideDemo2 && <About />}
+          <hr />
+          <div>
+            <button onClick={() => this.hideComponent("showHideDemo1")}>
+              Click to show landing page
+            </button>
+            <button onClick={() => this.hideComponent("showHideDemo2")}>
+              Click to show stats page
+            </button>
+            </div>
+            </div>
+        );
+      };
+    }
+  
+      
+  render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -5,55 +5,13 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/index.scss";
 import App from "./App.jsx"
-import { render } from "react-dom";
 import Main from "./components/Main.jsx";
-import About from "./components/About.jsx";
+import { render } from "react-dom";
 
-class Holla extends Component {
-constructor() {
-    super();
-    this.state = {
-      name: "React",
-      showHideDemo1: false,
-      showHideDemo2: false
-    };
-    this.hideComponent = this.hideComponent.bind(this);
-}
 
-hideComponent(name) {
-  console.log(name);
-  switch (name) {
-    case "showHideDemo1":
-      this.setState({ showHideDemo1: !this.state.showHideDemo1 });
-      break;
-    case "showHideDemo2":
-      this.setState({ showHideDemo2: !this.state.showHideDemo2 });
-      break;
-    default:
-      
-  }
-}
 
-render () {
-    const { showHideDemo1, showHideDemo2 } = this.state;
-    return (
-    <div>
-        {showHideDemo1 && <Main />}
-        <hr />
-        {showHideDemo2 && <About />}
-        <hr />
-        <div>
-          <button onClick={() => this.hideComponent("showHideDemo1")}>
-            Click to show landing page
-          </button>
-          <button onClick={() => this.hideComponent("showHideDemo2")}>
-            Click to show stats page
-          </button>
-          </div>
-          </div>
-);
-}
-}
-
-render(<Holla/>, document.getElementById('root'));
+render(
+  <div>
+  <App /> <Main/>
+  </div>, document.getElementById('root'));
 serviceWorker.unregister();
