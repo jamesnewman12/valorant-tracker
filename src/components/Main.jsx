266f495c37cdd0fa48c7d41fa5de7cb5fa8bc9ae
jamesnewman12 <mantaa@gmail.com>
2020-09-14 02:57:17 -0400

    button clicks, renders About, then everything breaks- needs work

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,33 @@ import "../styles/background.css";
 import Button from "@material-ui/core/Button";
 import About from "./About.jsx";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 export default class Main extends React.Component {
-  handleClick(event) {
-    console.log("clicked");
+  constructor(props) {
+    super(props);
+    this.state = {
+      showComponent: false,
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
   }
+  
+  _onButtonClick() {
+    this.setState({
+      showComponent: true,
+    });
+  }
+
 
   render() {
     return (
-      <div id="my-background" className="background">
+       <div id="my-background" className="background">
         <div id="stars" />
         <div id="stars2" />
         <div id="stars3" />
@@ -21,21 +39,27 @@ export default class Main extends React.Component {
         <div id="stars5" />
         <div id="stars6" />
         <div className="top-container">
-          <div id="logo1" className="logo1">
-            <Button
-              onClick={this.handleClick}
-              style={{ backgroundColor: "transparent" }}
-            >
-              <div id="picture2" className="picture2">
-                <img src={valorantlogo} alt="logo1" />
-              </div>
-              <div id="backgroundText" className="backgroundText">
-                Real Time Stat Tracker
-              </div>
-            </Button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+
+
+
+<div id="picture2" className="picture2">
+      
+      <Button><img src={valorantlogo} alt="logo1" 
+      style={{ backgroundColor: "transparent" }}
+      onClick={this._onButtonClick}>
+        </img>
+      {this.state.showComponent ?
+         <About /> :
+         null
+      }
+      </Button>
+      <div id="backgroundText" className="backgroundText">
+      Real Time Stat Tracker
+    </div>
+
+</div>
+</div>
+</div>
+    )}
 }
