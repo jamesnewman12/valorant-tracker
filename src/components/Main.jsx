3a2f0a177d325f17cdd0a5a5b2453c28af0b2179
jamesnewman12 <mantaa@gmail.com>
2020-09-15 22:21:20 -0400

    centered still star issue

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HoverImage from "react-hover-image";
 import Valorantlogo from "./../img/Valorantlogo.png";
 import Valorantlogored from "./../img/Valorantlogored.png";
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 const Main: React.SFC = () => {
   return (
@@ -15,13 +15,11 @@ const Main: React.SFC = () => {
       <div id="stars6" />
       <div id="stars7" />
       <Row className="justify-content-md-center">
-        <Col xs={12} sm={4} md={4}>
           <HoverImage
             src={Valorantlogo}
             hoverSrc={Valorantlogored}
             onClick={() => (window.location.href = "/login")}
           />
-        </Col>
       </Row>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/index.scss";
 import App from "./Router.jsx";
 
