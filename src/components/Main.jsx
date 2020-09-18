00ec39f1a9cefbd1b1dae61a51389e22de1d55ef
jamesnewman12 <mantaa@gmail.com>
2020-09-17 20:21:37 -0400

    set state for imagehover onMouseEnter and onMouseout to change to red or black

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,32 @@
-import React, { Image, Component, styles } from "react";
+import React, { Component } from "react";
 import Valorantlogo from "./../img/Valorantlogo.png";
 import Valorantlogored from "./../img/Valorantlogored.png";
-import Starbackground from "./Starbackground.jsx";
-import { Row } from "react-bootstrap";
 
 export default class ImageHover extends Component {
+  state = {
+    img: Valorantlogo,
+  };
   render() {
     return (
-  <div>
-  <div className="ImageHover">
-      <div className="justify-content-md-center">
-         <img src={Valorantlogo} alt="ImageHover"
-          hoversrc={Valorantlogored}
-          onClick={() => (window.location.href = "/login")}/>
-          </div>
-    </div>
-     </div>
-    )
-  };
+      <div className="ImageHover">
+        <div className="justify-content-md-center">
+          <img
+            src={this.state.img}
+            alt="logo"
+            onMouseEnter={() => {
+              this.setState({
+                img: Valorantlogored,
+              });
+            }}
+            onMouseOut={() => {
+              this.setState({
+                img: Valorantlogo,
+              });
+            }}
+            onClick={() => (window.location.href = "/login")}
+          />
+        </div>
+      </div>
+    );
+  }
 }
