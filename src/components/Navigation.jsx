cc52eac9d8d37ed65d8d44ebdbc6b75528548489
jamesnewman12 <mantaa@gmail.com>
2020-09-18 23:03:25 -0400

    fixed navbar positioning, removed search button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -51,7 +51,7 @@ class Navigation extends React.Component {
     return (
         
       <div className="navbar navbar-expand-lg navbar-transparent bg-transparent transparent">
-        <a className="navbar-brand" href="/">Player Profile</a>
+        <a className="navbar-brand" href="/">"USER"</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -68,10 +68,6 @@ class Navigation extends React.Component {
                 <a className="dropdown-item" href="/">View Overall Stats</a>
               </NavDropdown>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </div>
     )
