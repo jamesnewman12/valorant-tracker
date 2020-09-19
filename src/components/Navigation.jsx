684ebc5aaa387e1618bf527fe3e43b7616fd6409
jamesnewman12 <mantaa@gmail.com>
2020-09-19 00:16:59 -0400

    added strong text for nav, welcome user

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -51,7 +51,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className="navbar navbar-expand-lg navbar-transparent bg-transparent transparent">
-        <a className="navbar-brand" href="/">"USER"</a>
+        <a className="navbar-brand" href="/"><strong>Welcome "USER"</strong></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
