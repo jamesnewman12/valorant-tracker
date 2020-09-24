d44a8f6262144e6b6710e847e9a8e7eb164b62f4
jamesnewman12 <mantaa@gmail.com>
2020-09-24 18:37:56 -0400

    did a build for testing purposes

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4bac0673c69cb8216b0c695edc226e0.js"
+  "/precache-manifest.9ab458c90ea3c4ccd9c385968042be66.js"
 );
 
 self.addEventListener('message', (event) => {
